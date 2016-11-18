1959e8130e70c0b76d591694d66df8e9174ae6b0
Jaap Smit <jaapsmit94@hotmail.com>
2016-11-18 15:38:27 +0100

    Stukje verder

--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -1,11 +1,19 @@
 import javafx.application.Application;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 public class MusicPlayer extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) {
+		String path = "file:///C:/Users/Student/workspace/PubSongDesktop/src/Muziek/TomOdell-Magnetised.mp3";
+		Media song = new Media(path);
+		System.out.println(song.getDuration().toSeconds());
+		MediaPlayer player = new MediaPlayer(song);
+		player.play();
 		primaryStage.show();
+		System.out.println(song.getDuration().toSeconds());
 	}
 	
 	public static void main(String[] args) {
